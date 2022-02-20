992a59cdd158638ffb74c7839ee7f791c7f6a3ac
chenxuetong <1412335438@qq.com>
2022-02-20 17:50:44 +0800

    客户端开发2

--- video/app/client/urls.py
+++ video/app/client/urls.py
@@ -7,6 +7,10 @@
 
 from django.urls import path
 from .views.base import Index
+from .views.video import ExVideo
+
 urlpatterns = [
-    path('',Index.as_view(),name='client_index')
+    path('',Index.as_view(),name='client_index'),
+    path('video/ex',ExVideo.as_view(),name='client_ex_video')
+
 ]
--- video/app/client/views/video.py
+++ video/app/client/views/video.py
@@ -4,3 +4,15 @@
 # @Time    : 2022/2/20 17:14
 # @FileName: video.py
 # @Software: PyCharm
+
+from django.views.generic import View
+from app.libs.base_render import render_to_response
+from app.models import Video
+from app.model.video import FromType
+class ExVideo(View):
+    TEMPLATE = 'client/video/video.html'
+
+    def get(self,request):
+        videos = Video.objects.exclude(from_to=FromType.custom.value)
+        data = {'videos':videos}
+        return render_to_response(request,self.TEMPLATE,data=data)
